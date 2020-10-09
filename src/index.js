32ccfd23d10f8f1cd7174db49526dc4691efa519
Muskan Jaiswal <muskanjaiswal710@gmail.com>
2020-10-10 02:01:44 +0530

    'sticky'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Board from './Board';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Board />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('react-container')
 );
 
 // If you want your app to work offline and load faster, you can change
